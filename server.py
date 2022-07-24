a0c17f1af8ac0e4dc77f81997abbc7117a4b7406
KevinOLR <103300651+KevinOLR@users.noreply.github.com>
2022-07-23 23:48:02 -0500

    Add files via upload

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/play')
+def ruta_1():
+    return render_template("index.html",num=3,color="blue")
+
+@app.route('/play/<int:num>')
+def ruta_2(num):
+    return render_template("index.html", num=num, color="blue")
+
+@app.route('/play/<int:num>/<string:color>')
+def ruta_3(num, color):
+    return render_template("index.html", num=num, color=color)
+
+if __name__=="__main__":
+    app.run(debug=True)
